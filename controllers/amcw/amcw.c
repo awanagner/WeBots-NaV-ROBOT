807985700f9c2c120e847f22f75ddb04fcda6bd3
Ana Wagner <93949347+awanagner@users.noreply.github.com>
2025-10-23 08:09:10 -0400

    Update amcw.c

--- controllers/amcw/amcw.c
+++ controllers/amcw/amcw.c
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *///*
 
 /*
  * Description: Simple controller to make an "Hello" mouvement.
@@ -110,4 +110,5 @@ int main(int argc, char **argv) {
   wb_robot_cleanup();
   return 0;
 }
+
 
